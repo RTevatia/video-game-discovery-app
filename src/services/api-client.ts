2f74f774dbfd8574529f7b24944cb56b3135b210
Rahul Tevatia <rtevatia2@gmail.com>
2026-02-03 19:24:27 -0500

    update gitignore

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'e06a1afea1d94221994b75d0b41d06fb'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "e06a1afea1d94221994b75d0b41d06fb",
+  },
+});
