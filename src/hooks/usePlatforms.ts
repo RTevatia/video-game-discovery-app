7dcb2149ffdb90cb628508896c36cfec586375c5
Rahul Tevatia <rtevatia2@gmail.com>
2026-02-23 17:41:14 -0500

    Refactor games hook to fetch data with React Query and remove legacy data hook

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import useData, { FetchResponse } from "./useData";
-import apiClient from "../services/api-client";
+import apiClient, { FetchResponse } from "../services/api-client";
+
 
 interface Platform {
   id: number;
