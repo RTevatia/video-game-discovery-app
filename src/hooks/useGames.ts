14206850cb0dd86e580c2ac8fd7213fee7acea94
Rahul Tevatia <rtevatia2@gmail.com>
2026-02-24 17:42:41 -0500

    Refactor: create lookup hooks

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import App, { GameQuery } from "../App";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
